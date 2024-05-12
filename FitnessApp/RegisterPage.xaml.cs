276cbe25c6f50ec8a33f9f61476e143c87ebc011
Eren Ali Koca <eren_ali_koca@hotmail.com>
2024-05-12 16:39:42 +0300

    Customer Registration screen created

--- FitnessApp/RegisterPage.xaml.cs
+++ FitnessApp/RegisterPage.xaml.cs
@@ -1,3 +1,8 @@
+using FitnessApp.EfCore;
+using FitnessApp.Models;
+using FitnessAppWebApi.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace FitnessApp;
 
 public partial class RegisterPage : ContentPage
@@ -8,7 +13,24 @@ public partial class RegisterPage : ContentPage
 	}
     private async void OnBackButton6Clicked(object sender, EventArgs e)
     {
+
         await Navigation.PushAsync(new LoginPage());
     }
-    
+
+    private async void SignUpButton_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            FitnessDbContext context = new FitnessDbContext();
+            var person = new UserClass { Username=UsernameEntry.Text,Password=PasswordEntry.Text,Email=EmailEntry.Text,Gender= PickerEntry.SelectedIndex };
+            context.Users.Add(person);
+            context.SaveChanges();
+            await DisplayAlert("Baþarý","Kayýt Baþarýlý", "Tamam");
+            await Navigation.PushAsync(new LoginPage());
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Hata", "Boþ alanlarý lütfen doldurun!", "Tamam");
+        }
+    }
 }
